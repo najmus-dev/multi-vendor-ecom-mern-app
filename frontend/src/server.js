443b3343bc472b4f4cadb167a3a75d0942384c7e
Najmus Saqib <najmusesaqib123@gmail.com>
2024-10-14 15:26:40 +0500

    backend url edit

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,4 @@
-export const server = "http://localhost:5000/api/v2";
+// export const server = "http://localhost:5000/api/v2";
+export const server = "https://multi-vendor-ecom-backend.vercel.app/api/v2";
 
 
