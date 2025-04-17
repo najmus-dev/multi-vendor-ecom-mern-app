1f3b5785ec7bec676b5c2074a0c8064c6497dba6
Najmus Saqib <najmusesaqib123@gmail.com>
2025-04-17 10:50:51 +0500

    last updates

--- frontend/src/pages/UserInbox.jsx
+++ frontend/src/pages/UserInbox.jsx
@@ -9,8 +9,8 @@ import { useNavigate } from "react-router-dom";
 import { AiOutlineArrowRight, AiOutlineSend } from "react-icons/ai";
 import { TfiGallery } from "react-icons/tfi";
 import styles from "../styles/styles";
-// const ENDPOINT = "https://socket-for-multivendor-ecom.onrender.com/";
-const ENDPOINT = "http://localhost:4000"; 
+const ENDPOINT = "https://socket-for-multivendor-ecom.onrender.com/";
+// const ENDPOINT = "http://localhost:4000"; 
 const socketId = socketIO(ENDPOINT, { transports: ["websocket"] });
 
 const UserInbox = () => {
--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "http://localhost:5000/api/v2";
+// export const server = "http://localhost:5000/api/v2";
 // export const server = "https://multi-vendor-ecom-backend.vercel.app/api/v2";
-// export const server = "https://multi-vendor-ecom-backend-najmus-saqibs-projects.vercel.app/api/v2";
+export const server = "https://multi-vendor-ecom-backend-najmus-saqibs-projects.vercel.app/api/v2";
